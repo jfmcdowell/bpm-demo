33b4d00ec2676dd3484b48a8560fc383537c904c
jfmcdowell <206422+jfmcdowell@users.noreply.github.com>
2022-08-31 16:40:48 +0000

    Implement cdk-nag and update deps

--- bpm_demo/bpm_demo_stack.py
+++ bpm_demo/bpm_demo_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Duration, Stack
+from aws_cdk import Aspects, Duration, RemovalPolicy, Stack
 from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as _lambda
@@ -6,6 +6,7 @@ from aws_cdk import aws_logs as logs
 from aws_cdk import aws_stepfunctions as stepfunctions
 from aws_cdk import aws_stepfunctions_tasks as sftasks
 from aws_solutions_constructs.aws_lambda_stepfunctions import LambdaToStepfunctions
+from cdk_nag import AwsSolutionsChecks, NagSuppressions
 from constructs import Construct
 
 
@@ -38,6 +39,8 @@ class BpmDemoStack(Stack):
             layers=[powertools_layer],
         )
 
+        submit_price_appeal_lambda.apply_removal_policy(RemovalPolicy.DESTROY)
+
         # Status Check Lambda definition
 
         status_lambda = _lambda.Function(
@@ -52,6 +55,8 @@ class BpmDemoStack(Stack):
             tracing=_lambda.Tracing.ACTIVE,
             layers=[powertools_layer],
         )
+
+        status_lambda.apply_removal_policy(RemovalPolicy.DESTROY)
 
         # State machine definitons
 
@@ -110,9 +115,14 @@ class BpmDemoStack(Stack):
                 layers=[powertools_layer],
             ),
             state_machine_props=stepfunctions.StateMachineProps(
-                definition=definition, timeout=Duration.minutes(5)
+                definition=definition,
+                timeout=Duration.minutes(5),
+                tracing_enabled=True,
             ),
         )
+
+        event_producer_lambda.state_machine.apply_removal_policy(RemovalPolicy.DESTROY)
+        event_producer_lambda.lambda_function.apply_removal_policy(RemovalPolicy.DESTROY)
 
         # IAM policy for Producer Lambda
 
@@ -124,13 +134,58 @@ class BpmDemoStack(Stack):
 
         # Define API Gateway REST API resource backed by producer lambda function
 
+        access_log_group = logs.LogGroup(self, "BPMAccessLogs")
+        access_log_group.apply_removal_policy(RemovalPolicy.DESTROY)
+
         api = apigw.LambdaRestApi(
             self,
             "SampleAPI to trigger Event Producer",
             description=" API Gateway REST API resource backed by producer lambda function",
             handler=event_producer_lambda.lambda_function,  # type: ignore
             proxy=False,
+            deploy_options=apigw.StageOptions(
+                access_log_destination=apigw.LogGroupLogDestination(access_log_group),
+                access_log_format=apigw.AccessLogFormat.json_with_standard_fields(
+                    caller=True,
+                    http_method=True,
+                    ip=True,
+                    protocol=True,
+                    request_time=True,
+                    resource_path=True,
+                    response_length=True,
+                    status=True,
+                    user=True,
+                ),
+                logging_level=apigw.MethodLoggingLevel.INFO,
+                data_trace_enabled=True,
+            ),
         )
+
+        api.apply_removal_policy(RemovalPolicy.DESTROY)
+
+        request_validator = apigw.RequestValidator(
+            self,
+            "MyRequestValidator",
+            rest_api=api,
+        )
+        request_validator.apply_removal_policy(RemovalPolicy.DESTROY)
 
         items = api.root.add_resource("items")
         items.add_method("POST")  # POST /items
+
+        Aspects.of(self).add(AwsSolutionsChecks())
+        NagSuppressions.add_stack_suppressions(
+            self, [{"id": "AwsSolutions-IAM4", "reason": "TODO: Stop using AWS managed policies."}]
+        )
+        NagSuppressions.add_stack_suppressions(
+            self, [{"id": "AwsSolutions-IAM5", "reason": "TODO: Remove Wildcards in IAM roles."}]
+        )
+        NagSuppressions.add_stack_suppressions(
+            self, [{"id": "AwsSolutions-SF1", "reason": "TODO: Log all events in Cloudwatch."}]
+        )
+        NagSuppressions.add_stack_suppressions(
+            self, [{"id": "AwsSolutions-APIG4", "reason": "Never going to implement this"}]
+        )
+        NagSuppressions.add_stack_suppressions(
+            self, [{"id": "AwsSolutions-COG4", "reason": "Never going to implement this"}]
+        )
